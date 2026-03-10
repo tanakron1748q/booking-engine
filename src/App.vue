e868e4df11d771ae40b716f9b394666565ca7c2f
tanakron1748q <tanakron1748@gmail.com>
2026-03-10 21:13:06 +0700

    Setup Project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+    <div class="test">
+      <h1> Hello world</h1>
+    </div>
+</template>
+
+<style scoped>
+
+.test {
+
+  background-color: aqua;
+}
+</style>
